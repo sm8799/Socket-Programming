f4b3ccbeb8d42ce48d3ba6f363dcab1179c50f9b
Shivam Marathe <46926915+sm8799@users.noreply.github.com>
2019-09-11 09:29:23 +0530

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -22,23 +22,4 @@ while True:
 	connectionsocket, addr = serversocket.accept()
 	start_new_thread(clientfun, (connectionsocket, addr))
 serversocket.close()
-'''
-def numbers_to_strings(argument): 
-	switcher = { 
-		0: "zero", 
-		1: "one", 
-		2: "two", 
-	} 
 
-	# get() method of dictionary data type returns 
-	# value of passed argument if it is present 
-	# in dictionary otherwise second argument will 
-	# be assigned as default value of passed argument 
-	return switcher.get(argument, "nothing") 
-
-# Driver program 
-if __name__ == "__main__": 
-	argument=0
-	c = numbers_to_strings(argument)
-print(c)
-'''
