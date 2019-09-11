073a023b95379a77ab2cfb22d6cabbbf1c2c8169
Shivam Marathe <46926915+sm8799@users.noreply.github.com>
2019-09-11 15:45:59 +0530

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -57,3 +57,14 @@ for i in dir_list:
 </body>
 </html>
 '''
+'''
+       general-header = Cache-Control            ; Section 14.9
+                      | Connection               ; Section 14.10
+                      | Date                     ; Section 14.18
+                      | Pragma                   ; Section 14.32
+                      | Trailer                  ; Section 14.40
+                      | Transfer-Encoding        ; Section 14.41
+                      | Upgrade                  ; Section 14.42
+                      | Via                      ; Section 14.45
+                      | Warning                  ; Section 14.46
+'''
